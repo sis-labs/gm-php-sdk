6cc87546eaafafd933cc4c76b68a9942871b6a02
ollie314 <mehdi.lefebvre@gmail.com>
2019-07-06 11:54:50 +0200

    test: add some test to validate the behavior of guzzle
    In order to check the behavior of the stack, I wanna check the behavior / capabilities
    of the guzzle library. This also allow me to get a bit familiar with it. Those tests are
    not correctly placed in the project and they probably fail since the target version of the
    library is 7 and i cannot check the implementation on my machine which has a php 5.6 installed.
    It remains to relocated test if they are relevant or use ideas in other implementations / tests.
    
    Signed-off-by: ollie314 <mehdi.lefebvre@gmail.com>

--- test/Client/SimpleTest.php
+++ test/Client/SimpleTest.php
@@ -7,8 +7,15 @@ use \Firebase\JWT\JWT;
 use Ramsey\Uuid\Uuid;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Handler\CurlHandler;
+use GuzzleHttp\Middleware;
 use GuzzleHttp\Client as HttpClient;
 use Psr\Http\Message\RequestInterface;
+
+use GuzzleHttp\Handler\MockHandler;
+
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Prs7\Response;
+use GuzzleHttp\Exception\RequestException;
 
 use Gma\ApiClient\Client\Simple;
 
@@ -36,9 +43,9 @@ class SimpleTest extends TestCase {
     $this->assertTrue(true);
   }
   
-  public function testUsage() {
+  public function testMiddlewareUsage() {
     // GIVEN
-    $url = "http://localhost:3003";
+    $url = "http://192.168.1.101:3003";
     $resource = "/reactions";
     
     // WHEN
@@ -55,6 +62,86 @@ class SimpleTest extends TestCase {
     $this->assertEquals("application/json", $c[0]);
     $r = json_decode($response->getBody());
     $this->assertEquals("this is a test", $r->content);
+  }
+
+  public function testMiddleware() {
+      // GIVEN
+      $url = "http://192.168.1.101:3003";
+      $resource = "/reactions";
+
+      $stack = new HandlerStack();
+      $stack->setHandler(new CurlHandler());
+      $stack->push(Middleware::mapRequest(function(RequestInterface $request) {
+          $request->withHeader('x-correlation-id', (string)Uuid::uuid4());
+          $request->withHeader('User-agent', 'gm-php-sdk');
+          $request->withHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c');
+      }));
+      $client = new HttpClient(['base_uri' => $url, 'handler' => $stack]);
+
+      // WHEN
+      $response = $client->reqest('GET', $resource);
+
+      // THEN
+      $this->assertEquals(200, $response->getStatusCode());
+      $c = \explode(';', $response->getHeader('content-type')[0]);
+      $this->assertEquals('application/json', $c[0]);
+      $r = json_decode($response->getBody());
+      $this->assertEquals('this is a test', $r->content);
+  }
+
+  public function testNamedMiddleware() {
+      // GIVEN
+      $url = "http://192.168.1.101:3003";
+      $resource = "/reactions";
+
+      $stack = new HandlerStack();
+      $stack->setHandler(new CurlHandler());
+      $stack->push(Middleware::mapRequest(function(RequestInterface $request) {
+          $request->withHeader('x-correlation-id', (string)Uuid::uuid4());
+      }), 'correlationId');
+      $stack->push(Middleware::mapRequest(function(RequestInterface $request) {
+          $request->withHeader('User-Agent', 'gm-php-sdk');
+          $request->withHeader('referer', 'caller-name');
+          $request->withHeader('content-type', 'application/json');
+          $request->withHeader('accept', 'application/json');
+      }), 'technical');
+      $stack->push(Middleware::mapRequest(function(RequestInterface $request) {
+          $request->withHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c');
+          $request->withHeader('x-sender-id', (string)Uuid::uuid4());
+      }), 'authorization');
+
+      $client = new HttpClient(['base_uri' => $url, 'handler' => $stack]);
+
+      // WHEN
+      $response = $client->request('GET', $resource);
+
+      // THEN
+      $this->assertEquals(200, $response->getStatusCode());
+      $c = \explode(';', $response->getHeader('content-type')[0]);
+      $this->assertEquals('application/json', $c[0]);
+      $r = json_decode($response->getBody());
+      $this->assertEquals('this is a test', $r->content);
+  }
+
+  public function testGuzzleMockHandler() {
+      // GIVEN
+      $url = 'http://192.168.1.101:3003';
+      $resource = '/reactions';
+
+      $mock = new MockHanlder([
+          new Response(200, ['content-type' => 'application/json']);
+      ]);
+
+      $stack = HandlerStack::create($mock);
+      $client = new HttpClient(['handler' => $stack]);
+      
+      // WHEN
+      $response = $client->request('GET', $resource);
+
+      // THEN
+      $this->assertEquals(200, $response->getStatusCode());
+      $c = \explode(';', $response->getHeader('content-type')[0]);
+      $this->assertEquals('application/json', $c[0]);
   }
 
 }
